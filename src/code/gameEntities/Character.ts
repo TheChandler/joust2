e5bfc4c061efbd00b4296add9bf15c4ba22a070a
TheChandler <ChandlerMiller@gmail.com>
2025-08-09 01:30:40 -0400

    character controller work

--- src/code/gameEntities/Character.ts
+++ src/code/gameEntities/Character.ts
@@ -14,6 +14,19 @@ interface CharacterState {
     update(input: Input): void;
     collide(entity: Entity): void;
 }
+enum walkingState {
+    LEFT_1,
+    LEFT_2,
+    LEFT_3,
+    RIGHT_1,
+    RIGHT_2,
+    RIGHT_3,
+    STANDING
+}
+const WALKING_SPEED_1 = 1.5;
+const WALKING_SPEED_2 = 4;
+const WALKING_SPEED_3 = 10;
+const DELAY_TIME = 50;
 
 export class Character extends Entity {
     static hasPosition = true;
@@ -24,6 +37,10 @@ export class Character extends Entity {
     state: CharacterState;
     sprite: DrawableSprite;
     velocity: Vector2;
+
+    walkingState: walkingState = walkingState.STANDING;
+    timer: number = 0;
+
 
     constructor({ position, velocity, id }) {
         super();
@@ -65,31 +82,32 @@ export class Character extends Entity {
         this.state.collide(entity);
     }
 
-    public handleInput(input: Input) {}
+    public handleInput(input: Input) { }
 }
 
 function makeAirState(character: Character): CharacterState {
+    character.velocity.y = -10
     return {
         update: (input: Input) => {
-            if (input.left) {
-                character.velocity.x += -1;
-            }
+            // if (input.left) {
+            //     character.velocity.x += -1;
+            // }
 
-            if (input.right) {
-                character.velocity.x += 1;
-            }
+            // if (input.right) {
+            //     character.velocity.x += 1;
+            // }
 
-            if (Math.abs(character.velocity.x) > 10) {
-                character.velocity.x = 10 * Math.sign(character.velocity.x);
-            }
-            if (input.jump) {
-                character.velocity.y += -2;
-            }
+            // if (Math.abs(character.velocity.x) > 10) {
+            //     character.velocity.x = 10 * Math.sign(character.velocity.x);
+            // }
+            // if (input.jump) {
+            //     character.velocity.y += -2;
+            // }
 
             if (Math.abs(character.velocity.x) < 0.4) {
                 character.velocity.x = 0;
             }
-            character.velocity.y += 0.3;
+            character.velocity.y += .8;
         },
         collide: (entity: Entity) => {
             if (entity instanceof Platform) {
@@ -102,23 +120,103 @@ function makeAirState(character: Character): CharacterState {
 }
 
 function makeGroundedState(character: Character): CharacterState {
+    character.walkingState = walkingState.STANDING;
     return {
         update: (input: Input) => {
-            if (input.left) {
-                character.velocity.x += -1;
-            }
-
-            if (input.right) {
-                character.velocity.x += 1;
-            }
+            character.timer++;
             if (input.jump) {
                 character.state = makeAirState(character);
-                character.velocity.y = -10;
+                return;
             }
 
-            if (Math.abs(character.velocity.x) < 0.4) {
-                character.velocity.x = 0;
+            if (input.left) {
+                switch (character.walkingState) {
+                    case walkingState.LEFT_1:
+                        if (character.timer > DELAY_TIME) {
+                            character.walkingState = walkingState.LEFT_2;
+                            character.timer = 0;
+                        }
+                        break;
+                    case walkingState.LEFT_2:
+                        if (character.timer > DELAY_TIME) {
+                            character.walkingState = walkingState.LEFT_3;
+                            character.timer = 0;
+                        }
+                        break;
+                    case walkingState.LEFT_3:
+                        break;
+                    case walkingState.RIGHT_1:
+
+                    case walkingState.RIGHT_2:
+                    case walkingState.RIGHT_3:
+                        character.timer = 0;
+                        character.walkingState = walkingState.STANDING
+                        break;
+                    case walkingState.STANDING:
+                        if (character.timer > DELAY_TIME) {
+                            character.walkingState = walkingState.LEFT_1;
+                            character.timer = 0;
+
+                        }
+                        break;
+                }
+            } else if (input.right) {
+                switch (character.walkingState) {
+                    case walkingState.RIGHT_1:
+                        if (character.timer > DELAY_TIME) {
+                            character.walkingState = walkingState.RIGHT_2;
+                            character.timer = 0;
+                        }
+                        break;
+                    case walkingState.RIGHT_2:
+                        if (character.timer > DELAY_TIME) {
+                            character.walkingState = walkingState.RIGHT_3;
+                            character.timer = 0;
+                        }
+                        break;
+                    case walkingState.RIGHT_3:
+                        break;
+                    case walkingState.LEFT_1:
+                    case walkingState.LEFT_2:
+                    case walkingState.LEFT_3:
+                        character.walkingState = walkingState.STANDING
+                        character.timer = 0
+                        break;
+                    case walkingState.STANDING:
+                        if (character.timer > DELAY_TIME) {
+                            character.walkingState = walkingState.RIGHT_1;
+                            character.timer = 0;
+
+                        }
+                        break;
+                }
             }
+
+            switch (character.walkingState) {
+                case walkingState.LEFT_1:
+                    character.velocity.x = -WALKING_SPEED_1;
+                    break;
+                case walkingState.LEFT_2:
+                    character.velocity.x = -WALKING_SPEED_2;
+                    break;
+                case walkingState.LEFT_3:
+                    character.velocity.x = -WALKING_SPEED_3;
+                    break;
+                case walkingState.RIGHT_1:
+                    character.velocity.x = WALKING_SPEED_1;
+                    break;
+                case walkingState.RIGHT_2:
+                    character.velocity.x = WALKING_SPEED_2;
+                    break;
+                case walkingState.RIGHT_3:
+                    character.velocity.x = WALKING_SPEED_3;
+                    break
+                case walkingState.STANDING:
+                    character.velocity.x = 0;
+                    break;
+            }
+
+
         },
         collide: (entity: Entity) => {
             // Colliding with something while grounded
